84a78407601c0752aa4a9863d5a4613bc6484f8f
Adrian Gonzalez <Adrgon@Adrians-MacBook-Pro.local>
2020-04-02 17:46:25 -0300

    RestFull API Pizza

---
+++ index.js
@@ -0,0 +1,27 @@
+/*
++ Primary file for Pizza Delivery API
+*
+*/
+
+//Dependencies
+const server = require('./lib/server');
+const workers = require('./lib/workers');
+
+//Declare the app
+var app = {};
+
+//Init
+app.init = ()=>{
+
+	//Start the server
+	server.init();
+	
+	//Start the workers
+	workers.init();
+};
+
+//Execute 
+app.init();
+
+//Export the app
+module.exports = app;
